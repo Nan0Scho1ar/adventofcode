6616a475d2dd41f23255e2825c04bb5faea53118
nan0scho1ar <scorch267@gmail.com>
2023-12-05 18:02:08 +1000

    Updated comments

--- 2023/day-04/main(with-comments).rs
+++ 2023/day-04/main(with-comments).rs
@@ -12,21 +12,21 @@ fn main() {
     let (part1, part2, _) = read_to_string(INPUT_FILE)
         .unwrap()
         .lines()
-        .map(|line| re.captures_iter(line).count())
         // Count how many numbers match in each card. This is the "score" for that card.
+        .map(|line| re.captures_iter(line).count())
         .enumerate()
         // Init running totals to 0 and the card counts with 1 of each card.
         .fold((0, 0, vec![1; NUM_CARDS]), |(mut pt1, mut pt2, mut counts), (idx, score)| {
             // Part1: Score is 2^(n-1) where n is match count. Add it to the running total.
             pt1 += score.checked_sub(1).map_or(0, |len| 2u32.pow(len as u32));
-            // Part2: Add the count for the card to the running total.
+            // Part2: Add the count of this card to the running total.
             pt2 += counts[idx];
-            // Update the card counts based on the card we just processed.
+            // Update the card counts of the following cards based on this one.
             (idx+1..=idx+score).for_each(|c| counts[c] += counts[idx]);
-            //Pass accumulators through to next iteration.
+            //Pass accumulators through to next iteration to process the next card.
             (pt1, pt2, counts)
         });
 
-    println!("Part 1: {}", part1); // Answer: 21959
-    println!("Part 2: {}", part2); // Answer: 5132675
+    println!("Part 1: {}", part1);
+    println!("Part 2: {}", part2);
 }
